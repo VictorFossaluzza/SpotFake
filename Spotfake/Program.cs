03bfe4c0643375e2fea3cda7f6b5ebd804d8ae47
Victor Fossaluzza <victormota1998@gmail.com>
2023-06-27 22:12:34 -0300

    Melhorando o layout e entendendo um pouco mais sobre variávies do tipo texto

--- Spotfake/Program.cs
+++ Spotfake/Program.cs
@@ -3,9 +3,25 @@ string msgDeBoasVindas = "Boas vindas ao SpotFake";
 
 void ExibirMenssagemBoasVindas()
 {
-    Console.WriteLine("************************");
+    Console.WriteLine(@"
+                        ░██████╗██████╗░░█████╗░████████╗███████╗░█████╗░██╗░░██╗███████╗
+                        ██╔════╝██╔══██╗██╔══██╗╚══██╔══╝██╔════╝██╔══██╗██║░██╔╝██╔════╝
+                        ╚█████╗░██████╔╝██║░░██║░░░██║░░░█████╗░░███████║█████═╝░█████╗░░
+                        ░╚═══██╗██╔═══╝░██║░░██║░░░██║░░░██╔══╝░░██╔══██║██╔═██╗░██╔══╝░░
+                        ██████╔╝██║░░░░░╚█████╔╝░░░██║░░░██║░░░░░██║░░██║██║░╚██╗███████╗
+                        ╚═════╝░╚═╝░░░░░░╚════╝░░░░╚═╝░░░╚═╝░░░░░╚═╝░░╚═╝╚═╝░░╚═╝╚══════╝
+    ");
     Console.WriteLine(msgDeBoasVindas);
-    Console.WriteLine("************************");
+}
+
+void ExibirOpcoesDoMenu()
+{
+
+    Console.WriteLine("\nDigite 1 para registrar uma banda!");
+    Console.WriteLine("Digite 2 para listar todas as bandas!");
+    Console.WriteLine("Digite 3 para avaliar uma banda!");
+    Console.WriteLine("Digite 4 para exibir a nota média de uma banda!");
 }
 
 ExibirMenssagemBoasVindas();
+ExibirOpcoesDoMenu();
