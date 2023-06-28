c5473d421fd2b5685d9ff3d919e5751df11d799a
Victor Fossaluzza <victormota1998@gmail.com>
2023-06-27 22:21:47 -0300

    Implementando seleção no menu ,Variaveis do tipo inteiro. -> int.Parse( Convertendo string para int)

--- Spotfake/Program.cs
+++ Spotfake/Program.cs
@@ -22,6 +22,15 @@ void ExibirOpcoesDoMenu()
     Console.WriteLine("Digite 3 para avaliar uma banda!");
     Console.WriteLine("Digite 4 para exibir a nota média de uma banda!");
     Console.WriteLine("Digite 0 para sair");
+
+    Console.WriteLine("\n");
+    Console.Write("Digite sua opção:");
+    int opcaoEscolhida = int.Parse(Console.ReadLine()!);
+
+    if(opcaoEscolhida == 1)
+    {
+        Console.WriteLine("Você escolheu a opção " + opcaoEscolhida);
+    }
 }
 
 ExibirMenssagemBoasVindas();
